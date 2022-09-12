86af84872b2ad39b65344595c5f3813c7076d6d1
Amirtha <30748438+amirtha17@users.noreply.github.com>
2022-09-12 03:45:12 -0700

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -2,6 +2,8 @@
 public class Helloworld 
 {
 	
+        public static void main(String[] args)
+	{       System.out.println("Hello Subhashri"); 
 		public String Result() 
 		{
 		   System.out.println("Hello Subhashri");
